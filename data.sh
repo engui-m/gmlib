3bc0cfb8c040bb751abfed44b81ab764e354315a
guimmox <guimmox@users.noreply.github.com>
2021-05-04 17:12:47 -0300

    hive and mysql commands

--- data.sh
+++ data.sh
@@ -4,13 +4,23 @@ docker-compose up -d
 
 docker-compose start
 
-docker exec -it namenode bash
-
 docker-compose stop
 
+
+docker exec -it namenode bash
 
 hdfs dfs -ls /
 
 hdfs dfs -ls -R -h /user/hive/warehouse/
 
-hdfs dfs -put 
+hdfs dfs -put -h
+
+
+docker exec -it hive-server bash
+
+beeline -u jdbc:hive2://localhost:10000 # username password
+
+
+docker exec -it database bash
+
+mysql -h localhost -uroot -psecret
