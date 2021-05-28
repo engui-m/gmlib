5bce9682bad24648f860a74ece859a48b8540a95
guimmox <guimmox@users.noreply.github.com>
2021-05-28 19:42:36 -0300

    sets, hash

--- redis.sh
+++ redis.sh
@@ -79,8 +79,44 @@ del pesquisa:produto
  smembers pesquisa:produto_desconto
 
 
+del pesquisa:produto
+
+ zadd pesquisa:produto 100 monitor 20 HD 10 mouse 50 teclado
+
+ zcard pesquisa:produto
+
+ zrevrange pesquisa:produto 0 -1
+
+ zrevrank pesquisa:produto teclado
+
+ zscore pesquisa:produto teclado
+
+ zrem pesquisa:produto HD
+
+ zpopmax pesquisa:produto
+
+ zpopmin pesquisa:produto
+
+ zrange pesquisa:produto 0 -1
 
 
+del usuario:100
+ 
+ hmset usuario:100 nome Augusto estado SP views 10
 
+ hgetall usuario:100
 
+ hlen usuario:100
+
+ hmget usuario:100 nome views
+
+ hstrlen usuario:100 nome
+
+ hincrby usuario:100 views 2
+ 
+ hkeys usuario:100
+
+ hvals usuario:100
+
+ hdel usuario:100 estado
 
